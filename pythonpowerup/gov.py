95049ad6032416a3475cd21d06971ddb4c94ad2d
Vinicius Mendes Araujo <mendesaraujovinicius87@gmail.com>
2024-06-03 14:26:17 -0300

    codigo

---
+++ pythonpowerup/gov.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+pyautogui.press('win')
+pyautogui.write('excel')
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.press('')
